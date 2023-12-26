09077a4ce4688d0fccbe3dd3f43a9228670eb8fa
SherpaMilan <msherpa574@icloud.com>
2023-12-26 16:03:13 +1100

    Ran the tests, they passed. Now I'm wondering if the tests are on vacation

---
+++ src/App.test.js
@@ -0,0 +1,47 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import FeedbackForm from "./FeedbackForm";
+
+describe("Feedback Form", () => {
+  test("User is able to submit the form if the score is lower than 5 and additional feedback is provided", () => {
+    const score = "3";
+    const comment = "The pizza crust was too thick";
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    // You have to write the rest of the test below to make the assertion pass
+
+    //first user interaction to happen to set the score as 3
+    const range = screen.getByLabelText(/Score:/);
+    fireEvent.change(range, { target: { value: score } });
+
+    //second user interaction to happen with additional comment
+    const text = screen.getByLabelText(/Comments:/);
+    fireEvent.change(text, { target: { value: comment } });
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment,
+    });
+  });
+
+  test("User is able to submit the form if the score is higher than 5, without additional feedback", () => {
+    const score = "9";
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    // You have to write the rest of the test below to make the assertion pass
+    const range = screen.getByLabelText(/Score:/);
+    fireEvent.change(range, { target: { value: score } });
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment: "",
+    });
+  });
+});
